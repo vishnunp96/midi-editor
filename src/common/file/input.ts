40f712cd21ab52df624f11c26b84bbb77c933c1b
vnp23 <vnp23@imperial.ac.uk>
2024-05-23 20:41:30 +0100

    href to replace.

--- src/common/file/input.ts
+++ src/common/file/input.ts
@@ -23,18 +23,18 @@ async function handleFiles(files: FileList | null): Promise<void> {
   console.log("File content:" + midiData);
   const id = await cloudMidiRepository.uploadMidiData(midiData, files[0].name);
 
-  window.location.href = "/edit?id=" + id;
+  window.location.replace("/edit?id=" + id);
 }
 
 export function dragDropInput(e: DragEvent): void{
   e.preventDefault();
   if (e.dataTransfer){
-    handleFiles(e.dataTransfer.files).then(r => console.log("File handled"));
+    handleFiles(e.dataTransfer.files).then(_ => console.log("File handled"));
   }
 }
 
 export function uploadInput(e: Event): void{
     if (e.target instanceof HTMLInputElement){
-      handleFiles((e.target as HTMLInputElement).files).then(r => console.log("File handled"));
+      handleFiles((e.target as HTMLInputElement).files).then(_ => console.log("File handled"));
     }
 }
