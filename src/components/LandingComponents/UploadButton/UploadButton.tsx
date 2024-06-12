0a6034cf943c13930e5e71a37673237bc9472b8f
vnp23 <vnp23@imperial.ac.uk>
2024-06-12 15:30:19 +0100

    Reducing imports.

--- src/components/LandingComponents/UploadButton/UploadButton.tsx
+++ src/components/LandingComponents/UploadButton/UploadButton.tsx
@@ -3,28 +3,7 @@ import { uploadInput } from "../../../common/file/input"
 import { FC, useCallback } from "react"
 import { observer } from "mobx-react-lite"
 import { useStores } from "../../../main/hooks/useStores"
-import {
-  FileMenuButton
-} from "../../../main/components/Navigation/FileMenuButton"
-import { Tooltip } from "../../Tooltip"
-import { Localized } from "../../Localized"
-import { envString } from "../../../common/localize/envString"
-import PianoIcon from "../../../main/images/icons/piano.svg"
-import ArrangeIcon from "../../../main/images/icons/arrange.svg"
-import TempoIcon from "../../../main/images/icons/tempo.svg"
-import Settings from "mdi-react/SettingsIcon"
-import Help from "mdi-react/HelpCircleIcon"
-import Forum from "mdi-react/ForumIcon"
-import { UserButton } from "../../../main/components/Navigation/UserButton"
-import {
-  IconStyle,
-  Tab,
-  TabTitle
-} from "../../../main/components/Navigation/Navigation"
-import {
-  ICloudMidiRepository
-} from "../../../repositories/ICloudMidiRepository"
-import HomeRouter from "../../../main/stores/HomeRouter"
+
 
 const ButtonArea = styled.div`
     position: relative;
@@ -52,29 +31,9 @@ const InputElement = styled.input`
     z-index: 1;
 `
 
-// const UploadButton = () => {
-//   return (
-//     <ButtonArea className="upload-option" >
-//       {/*<InputElement type="file" accept="audio/midi" className="upload-input" onChange={uploadInput} />*/}
-//       {/*  <PlusImg src={"landing-plus.svg"} alt="Upload MIDI"/>*/}
-//       {/*  <div>*/}
-//       {/*    <h2>Upload MIDI</h2>*/}
-//       {/*  </div>*/}
-//     </ButtonArea>
-//   )
-//
-// }
-
 export const UploadButton: FC = observer(() => {
   const rootStore = useStores()
 
-
-  // const function uploadButtonInput(e: React.DragEvent, cloudMidiRepository: ICloudMidiRepository, homeRouter: HomeRouter): void{
-  //   e.preventDefault();
-  //   if (e.dataTransfer){
-  //     handleFiles(e.dataTransfer.files, cloudMidiRepository, homeRouter).then(_ => console.log("File handled"));
-  //   }
-  // }
   const handleButtonInput = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     uploadInput(e, rootStore)
   }, [rootStore])
